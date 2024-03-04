f9ed11c526c43d03c8d694c3fa1bd9123c4dcf8c
Darko Živić <zivic.darko79@gmail.com>
2024-03-04 22:29:13 +0100

    modified file

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -81,7 +81,7 @@ const CartDropdown = ({
           <LocalizedClientLink
             className="hover:text-ui-fg-base"
             href="/cart"
-          >{`Cart (${totalItems})`}</LocalizedClientLink>
+          >{`Košarica (${totalItems})`}</LocalizedClientLink>
         </Popover.Button>
         <Transition
           show={cartDropdownOpen}
