df4f78f4cf6d2efdf9ad1ef5a7b30199dbb9e3c9
Darko Živić <zivic.darko79@gmail.com>
2024-03-06 18:10:08 +0100

    Add responsive  design

--- src/modules/home/components/CategoryBanner.tsx
+++ src/modules/home/components/CategoryBanner.tsx
@@ -35,31 +35,32 @@ const categories: CategoryItem[] = [
   
   const CategoryBanner: React.FC = () => {
     return (
-        <>
-        <h3 className='grid align-baseline text-left text-2xl ml-7 my-10'>Izaberi kategoriju</h3>
-      <div className="grid grid-cols-6 gap-x-4 gap-y-6 pt-10 mx-4 px-4">
-        {categories.map((category) => (
-          <Link key={category.name}
-                href={category.href}
-                passHref
-                className="block relative hover:brightness-110 hover:scale-105 transition duration-300">
-            
-              <Image src={category.imageSrc} 
-                     alt={category.name} 
-                     width={500} 
-                     height={500}
-                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                     priority={false}
-                     className='object-cover rounded-xl' />
-              <div 
-                 className="absolute bottom-0 bg-black bg-opacity-20 w-full text-white text-center py-2 rounded-xl">{category.name}
-              </div>
-            
-          </Link>
-        ))}
-      </div>
+      <>
+        <h3 className='text-2xl ml-7 my-10 text-left'>Izaberi kategoriju</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 pt-10 mx-4 px-4">
+          {categories.map((category) => (
+            <Link key={category.name}
+                  href={category.href}
+                  passHref
+                  className="block relative hover:brightness-110 hover:scale-105 transition duration-300">
+              
+                <Image src={category.imageSrc} 
+                       alt={category.name} 
+                       width={500} 
+                       height={500}
+                       sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                       priority={false}
+                       className='object-cover rounded-xl' />
+                <div 
+                   className="absolute bottom-0 bg-black bg-opacity-20 w-full text-white text-center py-2 rounded-xl">{category.name}
+                </div>
+              
+            </Link>
+          ))}
+        </div>
       </>
     );
   };
+  
   
   export default CategoryBanner;
