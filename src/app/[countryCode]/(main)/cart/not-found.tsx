af213e34ca1e7adf97a2dfbf48c83de8f063d0c3
Darko Živić <zivic.darko79@gmail.com>
2024-04-09 21:43:47 +0200

    Modified text.

--- src/app/[countryCode]/(main)/cart/not-found.tsx
+++ src/app/[countryCode]/(main)/cart/not-found.tsx
@@ -10,12 +10,11 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">Stranica nije pronađena</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The cart you tried to access does not exist. Clear your cookies and try
-        again.
+       Stranica koju tražite nije pronađena. Očistite predmemoriju i probajte ponovo.
       </p>
-      <InteractiveLink href="/">Go to frontpage</InteractiveLink>
+      <InteractiveLink href="/">Idi na naslovnu stranicu</InteractiveLink>
     </div>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,15 +11,15 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">Stranica nije pronađena</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+        Stranica koju trežite ne postoji.
       </p>
       <Link
         className="flex gap-x-1 items-center group"
         href="/"
       >
-        <Text className="text-ui-fg-interactive">Go to frontpage</Text>
+        <Text className="text-ui-fg-interactive">Idi na naslovnu stranicu.</Text>
         <ArrowUpRightMini
           className="group-hover:rotate-45 ease-in-out duration-150"
           color="var(--fg-interactive)"
